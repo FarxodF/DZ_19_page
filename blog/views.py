67140bd7b1845b767913c660a023a4a2c9b6529f
Your Name <you@example.com>
2024-11-23 12:49:58 +0300

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    items_per_page = request.GET.get('items_per_page', 5)  # Получаем количество элементов на странице (по умолчанию 5)
-    paginator = Paginator(posts, items_per_page)
+    posts = Post.objects.all().order_by('-created_at')  # Получаем все посты, отсортированные по дате
+    paginator = Paginator(posts, 10)  # Показываем 10 постов на одной странице
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'blog/post_list.html', {'page_obj': page_obj, 'items_per_page': items_per_page})
-
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'blog/post_list.html', context)
